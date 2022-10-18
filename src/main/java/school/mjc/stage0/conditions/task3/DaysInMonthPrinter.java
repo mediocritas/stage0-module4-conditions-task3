8c0a31bb548c4dcfc8b5751fc9d96a41879e8c15
Olga Pushkina <emmanuilaklein@gmail.com>
2022-10-18 15:13:33 +0300

    All is done

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -1,6 +1,29 @@
 package school.mjc.stage0.conditions.task3;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        Map<Integer, Integer> months = new HashMap<Integer, Integer>();
+        months.put(1, 31);
+        months.put(2, 28);
+        months.put(3, 31);
+        months.put(4, 30);
+        months.put(5, 31);
+        months.put(6, 30);
+        months.put(7, 31);
+        months.put(8, 31);
+        months.put(9, 30);
+        months.put(10, 31);
+        months.put(11, 30);
+        months.put(12, 31);
+
+        if (!months.containsKey(month)) {
+            System.out.println("wrong number!");
+        } else {
+            System.out.println(months.get(month));
+        }
+
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -1,7 +1,26 @@
 package school.mjc.stage0.conditions.task3;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
+        Map<Integer, String> months = new HashMap<Integer, String>();
+        months.put(1, "Winter");
+        months.put(2, "Winter");
+        months.put(3, "Spring");
+        months.put(4, "Spring");
+        months.put(5, "Spring");
+        months.put(6, "Summer");
+        months.put(7, "Summer");
+        months.put(8, "Summer");
+        months.put(9, "Autumn");
+        months.put(10, "Autumn");
+        months.put(11, "Autumn");
+        months.put(12, "Winter");
+
+
+        System.out.println(months.getOrDefault(month, "wrong number!"));
 
     }
 }
